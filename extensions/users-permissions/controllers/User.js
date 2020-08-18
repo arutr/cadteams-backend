acfbb26c14952950a2b280ac5d1fed307cec15ec
Artur Komoter <artur@komoter.pl>
2020-08-18 16:04:58 +0200

    Secure user updating endpoints
    + Add password changing support

--- extensions/users-permissions/controllers/User.js
+++ extensions/users-permissions/controllers/User.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 // users/me with media and relational fields
 const { sanitizeEntity } = require('strapi-utils');
 
@@ -24,18 +25,52 @@ module.exports = {
    * @return {Object}
    */
   async updateMe(ctx) {
-    const { user } = ctx.state;
+    const {
+      request: {
+        body,
+      },
+      state: {
+        user,
+      },
+    } = ctx;
 
     if (!user) {
       return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
     }
 
+    const data = _.omit(body, [
+      'blocked',
+      'confirmed',
+      'email',
+      'provider',
+      'type',
+      'role',
+      'resetPasswordToken',
+      'verified',
+    ]);
+
+    if (body.password) {
+      const currentPassword = await strapi.plugins['users-permissions'].services.user.validatePassword(
+        body.currentPassword,
+        user.password,
+      );
+
+      if (!currentPassword) {
+        throw strapi.errors.badRequest(null, {
+          errors: [
+            { message: 'Invalid current password' },
+          ],
+        });
+      }
+
+      delete body.currentPassword;
+    }
+
     const updatedData = await strapi.plugins['users-permissions'].services.user.edit(
       { id: user.id },
-      { ...ctx.request.body },
+      data,
     );
 
-    const data = sanitizeUser(updatedData);
-    ctx.send(data);
+    ctx.send(sanitizeUser(updatedData));
   },
 };
