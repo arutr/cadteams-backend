d36a5529594fdfcf42212eddc6c1e27a28a338da
Artur Komoter <artur@komoter.pl>
2020-08-18 16:04:58 +0200

    Secure user updating endpoints
    + Add password changing support

--- config/plugins.js
+++ config/plugins.js
@@ -7,8 +7,8 @@ module.exports = ({ env }) => ({
       amazon: 'https://email.eu-west-2.amazonaws.com',
     },
     settings: {
-      defaultFrom: 'hello@cadteams.com',
-      defaultReplyTo: 'hello@cadteams.com',
+      defaultFrom: 'CADteams Support <hello@cadteams.com>',
+      defaultReplyTo: 'CADteams Support <hello@cadteams.com>',
     },
   },
 });
